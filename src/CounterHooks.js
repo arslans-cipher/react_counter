896096f7d3c8b9c06a5466cd27dbe5045ca58597
Arslan <ali.arslan319@gmail.com>
2022-11-05 23:21:30 -0400

    use single value instead of object to define state

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -2,16 +2,12 @@ import { useState } from 'react'
 
 const CounterHooks = ( {initialCount} ) => {
 
-  const [state, setState] = useState( { count: initialCount } )
+  const [count, setCount] = useState(initialCount)
 
   return <>
-    <button onClick={() => setState( prevState => { 
-        return { count: prevState.count - 1 }
-      })}>-</button>
-    <span>{state.count}</span>
-    <button onClick={() => setState( prevState => { 
-      return { count: prevState.count + 1}
-      })}>+</button>
+    <button onClick={() => { setCount( prevCount => prevCount - 1 )}}>-</button>
+    <span>{count}</span>
+    <button onClick={() => { setCount( prevCount => prevCount + 1 )}}>+</button>
   </>
 
 }
