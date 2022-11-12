f7915be20a374ff52e475571055b20fbd745ba4e
Arslan <ali.arslan319@gmail.com>
2022-11-11 20:28:03 -0500

    delete components to prepare for next phase of project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
-import React, { useState } from 'react'
-import Counter from "./Counter"
-import CounterHooks from "./CounterHooks"
-
-export const ThemeContext = React.createContext()
 
 const App = () => {
-
-  const [theme, setTheme] = useState('green')
-
-  return <ThemeContext.Provider value={ {backgroundColor: theme }}>
-    Counter
-    <Counter initialCount={0}/>
-    <br/>
-    <br/>
-    <br/>
-    Counter Hooks
-    <CounterHooks initialCount={0}/>
-    <br/>
-    <br/>
-    <br/>
-    <button onClick={() => setTheme(prevTheme => {
-      return prevTheme === 'red' ? 'blue' : 'red'})}>
-        Toggle Theme
-      </button>
-    </ThemeContext.Provider>
+  return null
 }
 
 export default App
