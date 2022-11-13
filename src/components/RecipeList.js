9b74b0440c93471e8375987916aa844ed5a5c10e
Arslan <ali.arslan319@gmail.com>
2022-11-13 01:05:03 -0500

    small edits to RecipeList.js to prepare for stylizing

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,16 +2,21 @@ import Recipe from "./Recipe"
 
 const RecipeList = ({ recipes }) => {
   return <>
-    <div>
-      {recipes.map(recipe => {
-        return <Recipe
-          key={recipe.id} 
-          {...recipe}
-        />
-      })}
+    <div className='recipe-list'>
+      <div>
+        {recipes.map(recipe => {
+          return <Recipe
+            key={recipe.id}
+            {...recipe}
+          />
+        })}
+      </div>
+      <div className='recipe-list__add-recipe-btn-container'>
+        <button className='btn btn--primary'>Add Recipe</button>
+      </div>
     </div>
-    <button>Add Recipe</button>
   </>
+
 }
 
 export default RecipeList
