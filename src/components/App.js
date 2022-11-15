6c46c78b9f9aa2627beccaf8574dd4a5e99bcda4
Arslan <ali.arslan319@gmail.com>
2022-11-14 23:35:17 -0500

    fix bug by refactoring code to avoid React18 restrictions

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,57 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { v4 as uuidv4 } from 'uuid'
 import RecipeList from "./RecipeList"
 import '../css/app.css'
 
 export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes'
+
+const App = () => {
+
+  const [recipes, setRecipes] = useState(() => { 
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (recipeJSON == null) {
+      return sampleRecipes
+    } else {
+      return JSON.parse(recipeJSON)
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
+  const handleRecipeAdd = () => {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instructions',
+      ingredients: [{
+        id: uuidv4(),
+        name: 'Name',
+        amount: '1 tablespoon'
+      }]
+    }
+    setRecipes([...recipes, newRecipe])
+  }
+
+  const handleRecipeDelete = (id) => {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete
+  }
+
+  return <>
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
+  </>
+}
 
 const sampleRecipes = [
   {
@@ -45,42 +93,5 @@ const sampleRecipes = [
     ]
   }
 ]
-
-const App = () => {
-
-  const [recipes, setRecipes] = useState(sampleRecipes)
-
-  const handleRecipeAdd = () => {
-    const newRecipe = {
-      id: uuidv4(),
-      name: 'New',
-      servings: 1,
-      cookTime: '1:00',
-      instructions: 'Instructions',
-      ingredients: [{
-        id: uuidv4(),
-        name: 'Name',
-        amount: '1 tablespoon'
-      }]
-    }
-    setRecipes([...recipes, newRecipe])
-  }
-
-  const handleRecipeDelete = (id) => {
-    setRecipes(recipes.filter(recipe => recipe.id !== id))
-  }
-
-   
-  const recipeContextValue = {
-    handleRecipeAdd,
-    handleRecipeDelete
-  }
-
-  return <>
-    <RecipeContext.Provider value={recipeContextValue}>
-      <RecipeList recipes={recipes} />
-    </RecipeContext.Provider>
-  </>
-}
 
 export default App
