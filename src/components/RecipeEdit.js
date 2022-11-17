5828d2f0c3a6af04c40ad710c65c4a9c9781a406
Arslan <ali.arslan319@gmail.com>
2022-11-16 23:55:39 -0500

    add grids to RecipeEdit

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -6,6 +6,28 @@ const RecipeEdit = () => {
       <div className='recipeEdit'>
         <button>&times;</button>
       </div>
+      <div>
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' id='name' />
+        <label htmlFor='cookTime'>Cook Time</label>
+        <input type='text' name='cookTime' id='cookTime' />
+        <label htmlFor='servings'>Serving</label>
+        <input type='number' min='1' name='servings' id='servings' />
+        <label htmlFor='instructions'>Instructions</label>
+        <textarea name='instructions' id='instructions'></textarea>
+      </div>
+      <br />
+      <label>Ingredients</label>
+      <div>
+        <div>Name</div>
+        <div>Amount</div>
+        <div></div>
+        {/* <RecipeIngredientEdit />
+        <RecipeIngredientEdit /> */}
+      </div>
+      <div>
+        <button>Add Ingredient</button>
+      </div>
     </div>
   </>
 }
