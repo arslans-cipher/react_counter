cb92842b14f9e00abd2a17e14a69cfd8ff04e328
Arslan <ali.arslan319@gmail.com>
2022-11-19 17:10:38 -0500

    allow for editing of ingredients to replace current state with user input

--- src/components/RecipeIngredientEdit.js
+++ src/components/RecipeIngredientEdit.js
@@ -1,8 +1,22 @@
+const RecipeIngredientEdit = ({ ingredient, handleIngredientChange }) => {
 
-const RecipeIngredientEdit = ({ ingredient }) => {
+  const handleChange = changes => {
+    handleIngredientChange(ingredient.id, {...ingredient, ...changes})
+  }
+
   return <>
-    <input className="recipe-edit__input" type="text" defaultValue={ingredient.name}/>
-    <input className="recipe-edit__input" type="text" defaultValue={ingredient.amount} />
+    <input 
+    className="recipe-edit__input" 
+    type="text" 
+    defaultValue={ingredient.name}
+    onChange={event => handleChange( {name: event.target.value} )}
+    />
+    <input 
+    className="recipe-edit__input" 
+    type="text" 
+    defaultValue={ingredient.amount} 
+    onChange={event => handleChange( {amount: event.target.value} )}
+    />
     <button className="btn btn--danger">&times;</button>
   </>
 }
