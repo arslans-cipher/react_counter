dee3e6755e9fd8df799dc73c9fdd00626db89a5f
Arslan <ali.arslan319@gmail.com>
2022-12-17 17:27:36 -0500

    css styling

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,14 +1,16 @@
 import Recipe from './Recipe'
 
 const RecipeList = ({ recipes }) => {
-  return <>
+  return <div className='recipe-list'>
     <div>
       {recipes.map(recipe => {
         return <Recipe key={recipe.id} {...recipe} />
       })}
     </div>
-    <button>Add Recipe</button>
-  </>
+    <div className='recipe-list__add-recipe-btn-container'>
+      <button className='btn btn--primary'>Add Recipe</button>
+    </div>
+  </div>
 }
 
 export default RecipeList
